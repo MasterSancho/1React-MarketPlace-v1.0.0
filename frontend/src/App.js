401c6e12083084c28f537552b530dbeca3ca10de
Alex M <1maltsevalex@gmail.com>
2022-12-13 19:47:43 +0200

    003-->Front End

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
  return (
   <>
-   <Header />
-   <main className='py-3'>
-    <Container>
-     <HomeScreen />
-    </Container>
-   </main>
-   <Footer />
+   <Router>
+    <Header />
+    <main className='py-3'>
+     <Container>
+      <Routes>
+       <Route path='/' element={<HomeScreen />} />
+       <Route path='/product/:id' element={<ProductScreen />} />
+      </Routes>
+     </Container>
+    </main>
+    <Footer />
+   </Router>
   </>
  );
 };
